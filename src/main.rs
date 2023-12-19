b818225f8cc42728e3b606a8aa4f5b989795e842
Tun <64010363@kmitl.ac.th>
2023-12-20 02:33:23 +0700

    Standard Workspace 1

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,6 @@
-use std::string::String;
+mod host;
 
 fn main()
 {
-    let s1 = String::from("127.0.0.1");
-    println!("Hello, world!");
 
-    match Host::new(&s1, true)
-    {
-        Ok(host1) =>
-        {
-            println!("Success {}", host1.start_port);
-
-            host1.tcp_scan();
-        }
-        Err(_) =>
-        {
-            println!("Fail");
-        }
-    }
 }
