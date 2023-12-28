bdbf720d181d75f310584c68cc1699e20377eff4
Tun <64010363@kmitl.ac.th>
2023-12-28 19:55:38 +0700

    main 1

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,13 @@ mod host;
 
 fn main()
 {
+    let ip = String::from("127.0.0.1");
+    let h1 = host::Host::new(3, &ip);
 
+    match h1 {
+        Ok(h11) => {
+            h11.scan();
+        }
+        Err(_) => {}
+    }
 }
