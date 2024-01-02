3d89349f5a6e5a5e3f4b8ba701d056230eb5b94e
misterjune <sounddevcloud145@gmail.com>
2024-01-03 03:30:18 +0700

    host 1.2 (Complete setup thread 1)

--- src/host.rs
+++ src/host.rs
@@ -32,15 +32,29 @@ impl Host
         }
     }
 
-    fn thread_spawner(port_no: u16) -> Result<thread::JoinHandle<()>, std::io::Error>
+    fn thread_build(port_no: u16) -> Result<thread::JoinHandle<()>, std::io::Error>
     {
         let builder: thread::Builder;
 
-        builder = thread::Builder::new().name(port_no.to_string());
+        builder = thread::Builder::new().name(port_no.to_string()).stack_size(32 * 1024);
         return builder.spawn( || {
-            todo!();
+            println!("Here");
         });
     }
+
+	fn thread_handler_helper(port_no: u16, thread_handler: &mut Vec<thread::JoinHandle<()>>) -> bool
+	{
+		match Host::thread_build(port_no)
+		{
+			Ok(handler) => {
+				thread_handler.push(handler);
+				return false;
+			}
+			Err(_) => {
+				return true;
+			}
+		}
+	}
 
     fn thread_joiner(thread_handler: Vec<thread::JoinHandle<()>>) -> Vec<Result<(), Box<(dyn Any + Send + 'static)>>>
     {
@@ -59,37 +73,28 @@ impl Host
     {
         let port_limit: (u16, u16);
         let mut thread_handlers: Vec<thread::JoinHandle<()>> = Vec::new();
-        let mut thread_results: Vec<Result<(), Box<(dyn Any + Send + 'static)>>>;
+        let mut _thread_results: Vec<Result<(), Box<(dyn Any + Send + 'static)>>>;
 
         match self.scan_mode.get_portlist()
         {
             Some(portlist) => {
-                // for port in portlist
-                // {
-                //     handler = thread::spawn(move ||{
-                //         println!("{port}");
-                //     });
-                //     thread_handlers.push(handler);
-                // }
+                for port in portlist
+				{
+					if Host::thread_handler_helper(port, &mut thread_handlers) {
+						break;
+					}
+				}
             }
             None => {
                 port_limit = self.scan_mode.get_limit();
                 for i in port_limit.0..port_limit.1
                 {
-                    match Host::thread_spawner(i)
-                    {
-                        Ok(handler) => {
-                            thread_handlers.push(handler);
-                        }
-                        Err(_) => {
-                            eprintln!("Thread allocation failed, stop making more thread");
-                            break;
-                        }
-                    }
+					if Host::thread_handler_helper(i, &mut thread_handlers) {
+						break;
+					}
                 }
-                thread_results = Host::thread_joiner(thread_handlers);
-                // Wait for writing Error handler
             }
         }
+		_thread_results = Host::thread_joiner(thread_handlers);
     }
 }
