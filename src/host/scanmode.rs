5e5f1b1c3d14ecc9f965a5a8b54b7aac2665bc1e
misterjune <sounddevcloud145@gmail.com>
2024-01-04 04:18:52 +0700

    add testcase for subset_len in ScanMode

--- src/host/scanmode.rs
+++ src/host/scanmode.rs
@@ -13,15 +13,18 @@ pub struct ScanMode
 fn create_hashmap() -> HashMap<String, (u16, u16, Option<Vec<u16>>)>
 {
 	return HashMap::from([
-		("fullscan".to_string(), (0, 65535, None)),
-		("quickscan".to_string(), (0, 1023, None)),
+		("full".to_string(), (0, 65535, None)),
+		("quick".to_string(), (0, 1023, None)),
+		("debug_priority_check".to_string(), (0, 100, Some(vec![1]))),
 		("debug_vec_10_port".to_string(), (0, 0, Some(vec![1, 200, 3, 4, 5, 6, 7, 8, 9, 65]))),
 		("debug_vec_2_port".to_string(), (0, 0, Some(vec![1, 1000]))),
-		("debug_vec_16_port".to_string(), (0, 0, Some(vec![13, 312, 132, 323, 56, 7, 88, 999, 1000, 1100, 1200, 1313, 1441, 15, 160]))),
-		("debug_vec_17_port".to_string(), (0, 0, Some(vec![13, 312, 132, 323, 56, 7, 88, 999, 1000, 1100, 1200, 1313, 1441, 15, 160, 17]))),
-		("debug_limit_0..10,000_port".to_string(), (0, 10000, None)),
+		("debug_vec_16_port".to_string(), (0, 0, Some(vec![13, 312, 132, 323, 56, 7, 88, 999, 1000, 1100, 1200, 1313, 1441, 15, 160, 0]))),
+		("debug_vec_17_port".to_string(), (0, 0, Some(vec![13, 312, 132, 323, 56, 7, 88, 999, 1000, 1100, 1200, 1313, 1441, 15, 160, 17, 0]))),
+		("debug_limit_0..0_port".to_string(), (0, 0, None)),
+		("debug_limit_0..9999_port".to_string(), (0, 9999, None)),
 		("debug_limit_60000..65535_port".to_string(), (60000, 65535, None)),
 		("debug_limit_50000..50015_port".to_string(), (50000, 50015, None)),
+		("debug_limit_50000..50016_port".to_string(), (50000, 50016, None))
 	]);
 }
 
@@ -62,7 +65,7 @@ impl ScanMode
 				vec_len = portlist.len();
 			}
 			None => {
-				vec_len = usize::from(self.upper - self.lower + 1);
+				vec_len = usize::from(self.upper) - usize::from(self.lower) + 1;
 			}
 		}
 		ceil = if vec_len % self.partition_size > 0 { 1 } else { 0 };
@@ -83,16 +86,113 @@ mod test
 {
 	use super::*;
 
+	lazy_static!(
+		static ref FULL: String = "full".to_string();
+		static ref QUICK: String = "quick".to_string();
+		static ref PRIORITY: String = "debug_priority_check".to_string();
+		static ref VEC_10: String = "debug_vec_10_port".to_string();
+		static ref VEC_2: String = "debug_vec_2_port".to_string();
+		static ref VEC_16: String = "debug_vec_16_port".to_string();
+		static ref VEC_17: String = "debug_vec_17_port".to_string();
+		static ref LIM_0_0: String = "debug_limit_0..0_port".to_string();
+		static ref LIM_0_9999: String = "debug_limit_0..9999_port".to_string();
+		static ref LIM_60000_65535: String = "debug_limit_60000..65535_port".to_string();
+		static ref LIM_50000_50015: String = "debug_limit_50000..50015_port".to_string();
+		static ref LIM_50000_50016: String = "debug_limit_50000..50016_port".to_string();
+	);
+
 	#[test]
-	fn normal_test1() {
-		let mode: ScanMode = ScanMode::new(&"debug1".to_string()).unwrap();
-		assert_eq!(mode.get_subset(0), Vec::new());
+	fn normal_access()
+	{
+		let mode = ScanMode::new(&QUICK);
+		assert!(mode.is_ok());
 	}
 
 	#[test]
-	#[should_panic]
-	fn panic_test1() {
-		let mode: ScanMode = ScanMode::new(&"debug2".to_string()).unwrap();
-		assert_eq!(mode.get_subset(8), Vec::new());
+	fn no_key_access()
+	{
+		let mode = ScanMode::new(&"sddfd".to_string());
+		assert!(mode.is_err());
+	}
+
+	#[test]
+	fn priority_check()
+	{
+		let mode = ScanMode::new(&PRIORITY);
+		let res = mode.unwrap().subset_len();
+		assert_eq!(res, 1);
+	}
+
+	fn cmp_mode(mode: &String, b: usize)
+	{
+		let mode = ScanMode::new(mode);
+		let a = mode.unwrap().subset_len();
+		assert_eq!(a, b);
+	}
+
+	#[test]
+	fn subset_len_full()
+	{
+		cmp_mode(&FULL, 4096);
+	}
+
+	#[test]
+	fn subset_len_quick()
+	{
+		cmp_mode(&QUICK, 64);
+	}
+
+	#[test]
+	fn subset_len_vec_10()
+	{
+		cmp_mode(&VEC_10, 1);
+	}
+
+	#[test]
+	fn subset_len_vec_2()
+	{
+		cmp_mode(&VEC_2, 1);
+	}
+
+	#[test]
+	fn subset_len_vec_16()
+	{
+		cmp_mode(&VEC_16, 1);
+	}
+
+	#[test]
+	fn subset_len_vec_17()
+	{
+		cmp_mode(&VEC_17, 2);
+	}
+
+	#[test]
+	fn subset_len_lim_0_0()
+	{
+		cmp_mode(&LIM_0_0, 1);
+	}
+
+	#[test]
+	fn subset_len_lim_0_9999()
+	{
+		cmp_mode(&LIM_0_9999, 625);
+	}
+
+	#[test]
+	fn subset_len_lim_60000_65535()
+	{
+		cmp_mode(&LIM_60000_65535, 346);
+	}
+
+	#[test]
+	fn subset_len_lim_50000_50015()
+	{
+		cmp_mode(&LIM_50000_50015, 1);
+	}
+
+	#[test]
+	fn subset_len_lim_50000_50016()
+	{
+		cmp_mode(&LIM_50000_50016, 2);
 	}
 }
