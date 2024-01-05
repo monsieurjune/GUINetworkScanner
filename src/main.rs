8d91698341c815eedb17e97066741d945f0814b7
misterjune <sounddevcloud145@gmail.com>
2024-01-05 19:23:25 +0700

    remove garbage comment from host.rs and make main work

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,13 @@ mod host;
 fn main()
 {
     let ip = String::from("127.0.0.1");
-    let h1 = host::Host::new(3, &ip);
+    let h1 = host::Host::new(&String::from("full"), &ip);
 
     match h1 {
         Ok(h11) => {
-            h11.scan();
+			println!("Here we go");
+            let str = h11.tcp_scan();
+			println!("Result : {}", str);
         }
         Err(_) => {}
     }
