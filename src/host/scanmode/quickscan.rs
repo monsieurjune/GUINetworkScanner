9f8090313e990b646c78e70dd96975988d13aabf
misterjune <sounddevcloud145@gmail.com>
2024-01-05 19:18:40 +0700

    delete scan/ and scanmode/ due to unnessesary now

--- src/host/scanmode/quickscan.rs
+++
@@ -1,6 +0,0 @@
-use std::vec::Vec;
-
-pub fn quickscan() -> (u16, u16, Option<Vec<u16>>)
-{
-    return (0, 1023, None);
-}
--- src/host/scanmode/top_ten.rs
+++
@@ -1,6 +0,0 @@
-use std::vec::Vec;
-
-pub fn top_ten() -> (u16, u16, Option<Vec<u16>>)
-{
-    return (0, 0, None);
-}
