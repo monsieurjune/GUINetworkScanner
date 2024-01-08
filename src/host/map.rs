e590cd5c891cecb7074c48c9f9d4c81b9c6e1499
misterjune <sounddevcloud145@gmail.com>
2024-01-09 03:50:28 +0700

    fix too long TCP-Connect Scan

--- src/host/map.rs
+++ src/host/map.rs
@@ -9,7 +9,7 @@ pub fn tcp_map_create() -> Map
 	HashMap::from([
 		("full".to_string(), (0, 65535, None)),
 		("fast".to_string(), (0, 1023, None)),
-		("test".to_string(), (0, 0, Some(vec![20])))
+		("test".to_string(), (0, 0, Some(vec![135])))
 	])
 }
 
--- src/host/tcp_connect.rs
+++ src/host/tcp_connect.rs
@@ -1,19 +1,21 @@
 use std::net::{
 	IpAddr,
-	TcpStream
+	TcpStream,
+	SocketAddr,
 };
+use std::time::Duration;
 
 pub fn scan(ip: &IpAddr, subset: Vec<u16>) -> Vec<u16>
 {
-	let ip_with_prefix: String = ip.to_string() + ":";
+	let mut ip_with_port: SocketAddr;
 	let mut port_status: Vec<u16> = Vec::new();
-	let mut ip_with_port: String;
+	let time: Duration = Duration::new(0, 030_000_000);
 
 	for port in subset
 	{
-		ip_with_port = ip_with_prefix.clone() + &port.to_string();
-		match TcpStream::connect(ip_with_port) {
-			Ok(_) => { port_status.push(port); }
+		ip_with_port = SocketAddr::new(ip.clone(), port);
+		match TcpStream::connect_timeout(&ip_with_port, time) {
+			Ok(_) => port_status.push(port),
 			Err(_) => {}
 		}
 	}
