5392a32dea86a9b3ae785efcf51b92f5f7298ee7
nanonymoussu <nanon2546@yahoo.com>
2024-02-12 22:38:39 +0700

    bluetooth_loopback_remove

--- src/lister/main.rs
+++ src/lister/main.rs
@@ -1,32 +1,41 @@
+#![allow(dead_code)]
+#![allow(unused_variables)]
+#![allow(unused_imports)]
+
+use local_ip_address::{list_afinet_netifas, local_ip};
 use network_interface::{
-    NetworkInterface,
-    NetworkInterfaceConfig,
-    Addr::{
-        V4,
-        V6,
-    },
-    V4IfAddr,
-    V6IfAddr
-};
-use local_ip_address::{
-    list_afinet_netifas,
-    local_ip
+    Addr::{V4, V6},
+    NetworkInterface, NetworkInterfaceConfig, V4IfAddr, V6IfAddr,
 };
 extern crate json;
 
-// fn get_wlan(info: Vec<NetworkInterface>) -> NetworkInterface
-fn remove_loopback(interface_info: &Vec<NetworkInterface>) -> Vec<NetworkInterface>
-{
+fn remove_windows(interface_info: &Vec<NetworkInterface>) -> Vec<NetworkInterface> {
     let mut cleaned_info: Vec<NetworkInterface> = Vec::new();
 
-    for interface in interface_info
-    {
+    for interface in interface_info {
+        match interface.name.contains("Bluetooth") || interface.name.contains("Loopback") {
+            true => {}
+            false => {
+                cleaned_info.push(interface.clone());
+            }
+        }
+    }
+
+    return cleaned_info;
+}
+
+// fn get_wlan(info: Vec<NetworkInterface>) -> NetworkInterface
+fn remove_loopback(interface_info: &Vec<NetworkInterface>) -> Vec<NetworkInterface> {
+    let mut cleaned_info: Vec<NetworkInterface> = Vec::new();
+
+    for interface in interface_info {
         match interface.name.find("lo") {
             Some(pos) => {
                 if pos != 0 {
                     cleaned_info.push(interface.clone());
                 }
             }
+
             None => {
                 cleaned_info.push(interface.clone());
             }
@@ -35,13 +44,11 @@ fn remove_loopback(interface_info: &Vec<NetworkInterface>) -> Vec<NetworkInterfa
     return cleaned_info;
 }
 
-fn main()
-{
+fn main() {
     let mut info = NetworkInterface::show().unwrap();
     // println!("{:?}", info);
-    let info2 = remove_loopback(&info);
+    let info2 = remove_windows(&info);
     println!("{:?}", info2);
-
 
     let info1 = info.pop().unwrap();
     let addr = &info2[0].addr;
