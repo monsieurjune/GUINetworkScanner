67c9f7f0a5cebd0f78e3bd7f78d37f96b52c001f
misterjune <you@example.com>
2024-03-08 21:36:44 +0700

    forget to grab utils

--- utils/probe.py
+++ utils/probe.py
@@ -18,15 +18,28 @@ def get_ip_subset(interface_info, interface_name, subset_no):
     )
 
 
-def probe_subset(subset):
-    cmd = [r"./target/release/probe", json.dumps(obj=subset)]
+def probe_subset(subset, inter_addr, passwd):
+    cmd1 = [
+        "echo",
+        passwd,
+    ]
+    cmd2 = [
+        "sudo",
+        "-S",
+        r"./target/release/probe",
+        json.dumps(obj=subset),
+        inter_addr
+    ]
 
-    result = subprocess.Popen(args=cmd, stdout=subprocess.PIPE)
-    out, _ = result.communicate()
-    exit_code = result.wait()
+    print("Start : ", subset)
+    result1 = subprocess.Popen(args=cmd1, stdout=subprocess.PIPE)
+    output = subprocess.check_output(cmd2, stdin=result1.stdout)
+    print("End : ", output)
+    exit_code = result1.wait()
 
+    # print(out)
     return (
-        json.loads(str(object=out.decode(encoding="utf-8"))) if exit_code == 0 else None
+        json.loads(str(object=output.decode(encoding="utf-8"))) if exit_code == 0 else None
     )
 
 
@@ -45,13 +58,7 @@ def probe_subset(subset):
 
 
 def main() -> None:
-    str0 = '{"length":2,"interface":[{"index":0,"name":"wlan0","addr":"10.18.7.186","broadcast":"10.18.15.255","netmask":"255.255.240.0","mac":"A4:6B:B6:9C:22:2D"},{"index":1,"name":"docker0","addr":"172.17.0.1","broadcast":"172.17.255.255","netmask":"255.255.0.0","mac":"02:42:D8:91:83:1F"}]}'
-    str00 = json.loads(str0)
-    str1 = get_ip_subset(str00, "wlan0", 16)
-
-    print(str1)
-    # print(get_ip_subset(interface_info=str1, interface_name="wlan0", subset_no=256))
-    # print(probe_subset(set=str1, i="wlan0")
+    pass
 
 
 if __name__ == "__main__":
